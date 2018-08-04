97c42a13439f30dcb99151af9fb7d093afefe428
Gustavo <gustavo.batista@tivit.com>
2018-08-04 17:39:03 -0300

    Check alive

--- src/main/java/com/gfbdev/controller/ConsumptionController.java
+++ src/main/java/com/gfbdev/controller/ConsumptionController.java
@@ -5,6 +5,7 @@ import com.gfbdev.entity.Response;
 import com.gfbdev.entity.dto.ConsumptionDTO;
 import com.gfbdev.session.ConsumptionSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,16 +28,18 @@ public class ConsumptionController {
         return session.addConsumption(dto);
     }
 
-    @RequestMapping("/listConsumption")
-    public Response listConsumption(@RequestBody ConsumptionDTO dto){
-        return session.getConsumption(dto);
+    @RequestMapping("/getConsumption/{userId}/{providerId}")
+    public Response getConsumption(@PathVariable("userId") String userId, @PathVariable("providerId") String providerId) {
+        return session.getConsumption(userId, providerId);
     }
+
     @RequestMapping("/additem")
-    public Response additem(@RequestBody ConsumptionDTO dto){
+    public Response additem(@RequestBody ConsumptionDTO dto) {
         return session.addItem(dto);
     }
+
     @RequestMapping("/removeItem")
-    public Response removeItem(@RequestBody ConsumptionDTO dto){
+    public Response removeItem(@RequestBody ConsumptionDTO dto) {
         return session.removeItem(dto);
     }
 }
